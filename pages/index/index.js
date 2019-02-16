c5b922f8746a4514d01b252ec0e3ee7b4dd83e81
MichaelLee826 <michaelleef2008@gmail.com>
2019-02-16 16:53:52 +0800

    优化显示效果，继续完善布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,9 +11,12 @@ Page({
     mortgage_options: ["等额本息", "等额本金"],
     mortgage_duration: [],
     mortgage_ways: ["贷款金额", "住房面积"],
+    mortgage_interest: ["基准利率8.5折", "基准利率9折", "基准利率9.5折", "基准利率(4.9%)", "基准利率1.1倍", "基准利率1.2倍", "自定义利率"],
+    mortgage_payback_time: "",
     index_options: 0,
     index_duration: 0,
     index_ways: 0,
+    index_interest: 3,
     value: [3],
   },
 
@@ -32,11 +35,9 @@ Page({
     });
 
     var duration = [];
-    for (var i = 0; i < 20; i++){
+    for (var i = 0; i < 30; i++){
       duration[i] = (i + 1) + "年(" + (i + 1) * 12 + "期)";
     }
-    duration[20] = "25年(300期)";
-    duration[21] = "30年(360期)";
     
     that.setData({
       winHeight: height,
@@ -67,7 +68,21 @@ Page({
           index_ways: val
         });
         break;
+
+      case "picker_interest":
+        this.setData({
+          index_interest: val
+        });
+        break;
     }
+  },
+
+  //时间picker的监听器
+  bindTimePickerchange: function (e) {
+    let val = e.detail.value;
+    this.setData({
+      index_payback_time: val
+    });
   },
 
   //滑动切换
