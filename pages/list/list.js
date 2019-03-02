16a5cabc9f10fe07532fa6424a4119eb6506382d
MichaelLee826 <michaelleef2008@gmail.com>
2019-03-02 10:05:58 +0800

    完善 月供详情 列表显示

--- pages/list/list.js
+++ pages/list/list.js
@@ -3,9 +3,13 @@
 Page({
   data: {
     dataList: [],
+    scroll_height: 0,
   },
 
   onLoad: function (options) {
+    var windowHeight = wx.getSystemInfoSync().windowHeight // 屏幕的高度
+    var windowWidth = wx.getSystemInfoSync().windowWidth // 屏幕的宽度
+
     var that = this;
     var dataList = JSON.parse(options.dataList); 
 
@@ -16,6 +20,7 @@ Page({
 
     that.setData({
       dataList: dataList,
+      scroll_height: windowHeight * 750 / windowWidth - 100,
     });
   },
 })
