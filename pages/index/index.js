66b7c498d3f9fee7d8114bba48f3aaf694fe11a2
MichaelLee826 <michaelleef2008@gmail.com>
2019-06-01 15:29:07 +0800

    全部功能已完成，提交审核2.0版本

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,6 +12,7 @@ Page({
     mortgage_options: ["等额本息", "等额本金"],
     mortgage_duration: [],
     mortgage_ways: ["贷款金额", "住房面积"],
+    mortgage_ratio: ["20%", "25%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%", ],
     mortgage_interest_commercial: ["基准利率8.5折", "基准利率9折", "基准利率9.5折", "基准利率", "基准利率1.1倍", "基准利率1.2倍", "自定义利率"],
     mortgage_interest_HAF: ["基准利率", "基准利率1.1倍", "基准利率1.2倍", "自定义利率"],
     mortgage_payback_time: "",
@@ -20,6 +21,7 @@ Page({
     commercial_index_options: 0,
     commercial_index_duration: 19,
     commercial_index_ways: 0,
+    commercial_index_ratio: 2,
     commercial_index_interest: 3,
     commercial_startDate: "",
     commercial_date: "",
@@ -29,6 +31,9 @@ Page({
     commercial_total_show: true,
     commercial_area_show: false,
     commercial_total: -1,
+    commercial_price: -1,
+    commercial_area: -1,
+    commercial_ratio: 0.3,
     commercial_interest: 4.90,
     commercial_payback_time: "",
     commercial_interest_disable: true,
@@ -38,6 +43,7 @@ Page({
     HAF_index_options: 0,
     HAF_index_duration: 19,
     HAF_index_ways: 0,
+    HAF_index_ratio: 2,
     HAF_index_interest: 0,
     HAF_startDate: "",
     HAF_date: "",
@@ -47,6 +53,9 @@ Page({
     HAF_total_show: true,
     HAF_area_show: false,
     HAF_total: -1,
+    HAF_price: -1,
+    HAF_area: -1,
+    HAF_ratio: 0.3,
     HAF_interest: 3.25,
     HAF_payback_time: "",
     HAF_interest_disable: true,
@@ -200,6 +209,46 @@ Page({
         });
         break;
 
+      case "commercial_picker_ratio":
+        var ratio = -1.0;
+        switch (val) {
+          case "0":
+            ratio = 0.2;
+            break;
+          case "1":
+            ratio = 0.25;
+            break;
+          case "2":
+            ratio = 0.3;
+            break;
+          case "3":
+            ratio = 0.4;
+            break;
+          case "4":
+            ratio = 0.5;
+            break;
+          case "5":
+            ratio = 0.6;
+            break;
+          case "6":
+            ratio = 0.7;
+            break;
+          case "7":
+            ratio = 0.8;
+            break;
+          case "8":
+            ratio = 0.9;
+            break;
+          case "9":
+            ratio = 1.0;
+            break;
+        }
+        this.setData({
+          commercial_index_ratio: val,
+          commercial_ratio: ratio,
+        });
+        break;
+
         //公积金贷款 计算方式
       case "HAF_picker_ways":
         var HAF_total_show;
@@ -218,6 +267,46 @@ Page({
           HAF_way: that.data.mortgage_ways[val],
           HAF_total_show: HAF_total_show,
           HAF_area_show: HAF_area_show,
+        });
+        break;
+
+      case "HAF_picker_ratio":
+        var ratio = -1.0;
+        switch (val) {
+          case "0":
+            ratio = 0.2;
+            break;
+          case "1":
+            ratio = 0.25;
+            break;
+          case "2":
+            ratio = 0.3;
+            break;
+          case "3":
+            ratio = 0.4;
+            break;
+          case "4":
+            ratio = 0.5;
+            break;
+          case "5":
+            ratio = 0.6;
+            break;
+          case "6":
+            ratio = 0.7;
+            break;
+          case "7":
+            ratio = 0.8;
+            break;
+          case "8":
+            ratio = 0.9;
+            break;
+          case "9":
+            ratio = 1.0;
+            break;
+        }
+        this.setData({
+          HAF_index_ratio: val,
+          HAF_ratio: ratio,
         });
         break;
 
@@ -478,6 +567,30 @@ Page({
         }
         break;
 
+      case "commercial_input_price":
+        if (e.detail.value == "") {
+          that.setData({
+            commercial_price: -1
+          });
+        } else {
+          that.setData({
+            commercial_price: e.detail.value
+          });
+        }
+        break;
+
+      case "commercial_input_area":
+        if (e.detail.value == "") {
+          that.setData({
+            commercial_area: -1
+          });
+        } else {
+          that.setData({
+            commercial_area: e.detail.value
+          });
+        }
+        break;
+
       case "commercial_input_interest":
         if (e.detail.value == "") {
           that.setData({
@@ -498,6 +611,30 @@ Page({
         } else {
           that.setData({
             HAF_total: e.detail.value
+          });
+        }
+        break;
+
+      case "HAF_input_price":
+        if (e.detail.value == "") {
+          that.setData({
+            HAF_price: -1
+          });
+        } else {
+          that.setData({
+            HAF_price: e.detail.value
+          });
+        }
+        break;
+
+      case "HAF_input_area":
+        if (e.detail.value == "") {
+          that.setData({
+            HAF_area: -1
+          });
+        } else {
+          that.setData({
+            HAF_area: e.detail.value
           });
         }
         break;
@@ -572,6 +709,9 @@ Page({
     var combination_comm_duration;
     var combination_HAF_duration;
     var total;
+    var price;
+    var area;
+    var ratio;
     var combination_comm_total;
     var combination_HAF_total;
     var interest;
@@ -583,7 +723,14 @@ Page({
       case 0:
         option = that.data.commercial_option;
         duration = that.data.commercial_duration;
-        total = that.data.commercial_total;
+        if (that.data.commercial_way == "贷款金额") {
+          total = that.data.commercial_total;
+        } else {
+          price = that.data.commercial_price;
+          area = that.data.commercial_area;
+          ratio = that.data.commercial_ratio;
+          total = (price * area / 10000 * (1 - ratio)).toFixed(2);
+        }
         interest = that.data.commercial_interest;
         payback_time = that.data.commercial_payback_time;
         break;
@@ -591,7 +738,14 @@ Page({
       case 1:
         option = that.data.HAF_option;
         duration = that.data.HAF_duration;
-        total = that.data.HAF_total;
+        if (that.data.HAF_way == "贷款金额") {
+          total = that.data.HAF_total;
+        } else {
+          price = that.data.HAF_price;
+          area = that.data.HAF_area;
+          ratio = that.data.HAF_ratio
+          total = (price * area / 10000 * (1 - ratio)).toFixed(2);
+        }
         interest = that.data.HAF_interest;
         payback_time = that.data.HAF_payback_time;
         break;
@@ -613,33 +767,71 @@ Page({
 
     //商业贷款和公积金贷款
     if (that.data.currentTab == 0 || that.data.currentTab == 1) {
-      //判断贷款金额是否填写正确
-      if (total == -1) {
-        that.showAlert("请填写贷款金额！");
-      } else if (total == 0) {
-        that.showAlert("贷款金额不能为0！");
-      }
-      //if判断条件的写法是根据几个测试用例推出来的
-      else if (!(reg_int.test(total) ^ reg_float.test(total))) {
-        that.showAlert("请填写正确格式的贷款金额！");
-      }
+      //根据贷款金额计算
+      if (that.data.commercial_way == "贷款金额") {
+        //判断贷款金额是否填写正确
+        if (total == -1) {
+          that.showAlert("请填写贷款金额！");
+        } else if (total == 0) {
+          that.showAlert("贷款金额不能为0！");
+        }
+        //if判断条件的写法是根据几个测试用例推出来的
+        else if (!(reg_int.test(total) ^ reg_float.test(total))) {
+          that.showAlert("请填写正确格式的贷款金额！");
+        }
 
-      //判断自定义贷款利率是否填写正确
-      else if (interest == -1) {
-        that.showAlert("请填写贷款利率！");
-      } else if (interest == 0) {
-        that.showAlert("贷款利率不能为0！");
+        //判断自定义贷款利率是否填写正确
+        else if (interest == -1) {
+          that.showAlert("请填写贷款利率！");
+        } else if (interest == 0) {
+          that.showAlert("贷款利率不能为0！");
+        }
+        //if判断条件的写法是根据几个测试用例推出来的
+        else if (!(reg_int.test(interest) ^ reg_float.test(interest))) {
+          that.showAlert("请填写正确格式的贷款利率！");
+        } else {
+          wx.navigateTo({
+            url: '/pages/result/result?option=' + option + '&duration=' + duration + '&total=' + total + '&interest=' + interest + '&payback_time=' + payback_time
+          })
+        }
       }
-      //if判断条件的写法是根据几个测试用例推出来的
-      else if (!(reg_int.test(interest) ^ reg_float.test(interest))) {
-        that.showAlert("请填写正确格式的贷款利率！");
-      } else {
-        wx.navigateTo({
-          url: '/pages/result/result?option=' + option + '&duration=' + duration + '&total=' + total + '&interest=' + interest + '&payback_time=' + payback_time
-        })
+      //根据住房面积计算
+      else {
+        //判断单价和面积是否填写正确
+        if (price == -1) {
+          that.showAlert("请填写单价！");
+        } else if (price == 0) {
+          that.showAlert("单价不能为0！");
+        }
+        //if判断条件的写法是根据几个测试用例推出来的
+        else if (!(reg_int.test(price) ^ reg_float.test(price))) {
+          that.showAlert("请填写正确格式的单价！");
+        } else if (area == -1) {
+          that.showAlert("请填写面积！");
+        } else if (area == 0) {
+          that.showAlert("面积不能为0！");
+        }
+        //if判断条件的写法是根据几个测试用例推出来的
+        else if (!(reg_int.test(price) ^ reg_float.test(price))) {
+          that.showAlert("请填写正确格式的单价！");
+        }
+
+        //判断自定义贷款利率是否填写正确
+        else if (interest == -1) {
+          that.showAlert("请填写贷款利率！");
+        } else if (interest == 0) {
+          that.showAlert("贷款利率不能为0！");
+        }
+        //if判断条件的写法是根据几个测试用例推出来的
+        else if (!(reg_int.test(interest) ^ reg_float.test(interest))) {
+          that.showAlert("请填写正确格式的贷款利率！");
+        } else {
+          wx.navigateTo({
+            url: '/pages/result/result?option=' + option + '&duration=' + duration + '&total=' + total + '&interest=' + interest + '&payback_time=' + payback_time
+          })
+        }
       }
     }
-
     //组合贷款
     else {
       //判断贷款金额是否填写正确
